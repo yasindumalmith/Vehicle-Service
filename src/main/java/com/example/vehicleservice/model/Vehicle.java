b503d74c02e46c216df33711b5dc0a2daf477b44
Yasindu <yasindumalmith058@gmail.com>
2025-10-20 19:03:32 +0530

    Create 3 method in repository

--- src/main/java/com/example/vehicleservice/model/Vehicle.java
+++ src/main/java/com/example/vehicleservice/model/Vehicle.java
@@ -1,10 +1,7 @@
 package com.example.vehicleservice.model;
 
 
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -19,9 +16,13 @@ public class Vehicle {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long serviceId;
+    @Column(name="vehicleType",nullable=false)
     private String vehicleType;
+    @Column(name = "vehicleNumber",nullable=false)
     private String vehicleNumber;
+    @Column(name = "serviceYear", nullable=false)
     private int serviceYear;
+    @Column(name = "serviceDetails", nullable=false)
     private String serviceDetails;
 
 }
--- src/main/java/com/example/vehicleservice/repository/VehicleRepository.java
+++ src/main/java/com/example/vehicleservice/repository/VehicleRepository.java
@@ -1,5 +1,14 @@
 package com.example.vehicleservice.repository;
 
-public interface VehicleRepository {
+import com.example.vehicleservice.model.Vehicle;
+import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
+public interface VehicleRepository extends JpaRepository<Vehicle,Long> {
+    List<Vehicle> findByServiceYear(int serviceYear);
+
+    List<Vehicle> findVehicleTypeByServiceId(Long serviceId);
+    List<Vehicle> deleteServiceYear(int serviceYear);
 }
+
